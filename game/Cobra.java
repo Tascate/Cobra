a326764efb9da1b61ff60ea107ee2277e8f08f1c
Tascate <50305878+Tascate@users.noreply.github.com>
2019-05-05 17:53:59 -0700

    Draws controllable Circle
    Moveable inside window with the Arrow Keys

--- game/Cobra.java
+++ game/Cobra.java
@@ -7,61 +7,68 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
 public class Cobra extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	ShapeRenderer shapeRender;
 	FieldObject[][] grid;
 	Point char1;
 	Point char2;
 	Boolean paused;
+	int row = 100;
+	int col = 100;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		shapeRender = new ShapeRenderer();
 		paused = false;
-		grid = new FieldObject[12][12];
 		
+		grid = new FieldObject[Gdx.graphics.getWidth()][Gdx.graphics.getHeight()];
 		grid[3][6] = new Player(5,new Color(0,0,1,1), 90);
 		char1 = new Point(3,6);
-		
 		grid[9][6] = new Player(5,new Color(1,0,0,1), 90);
 		char2 = new Point(9,6);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 0, 1, 1);
+		Gdx.gl.glClearColor(0.3f, 0.3f, 0.4f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		checkForInput();
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		shapeRender.begin(ShapeType.Filled);
+		shapeRender.setColor(Color.BLUE);
+		shapeRender.circle(row, col, 5);
+		shapeRender.end();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		shapeRender.dispose();
 	}
 	
 	private void checkForInput() {
 		int row = (int)char1.getX();
 		int col = (int)char1.getY();
-		if(Gdx.input.isKeyJustPressed(Input.Keys.LEFT)){
+		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
             grid[row][col].setAngle(180);
+            System.out.println("Left!");
+            this.row -= 4; //testing moving circle
         }
-		if(Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)){
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
             grid[row][col].setAngle(0);
+            System.out.println("Right!");
+            this.row += 4;
         }
-		if(Gdx.input.isKeyJustPressed(Input.Keys.UP)){
+		if(Gdx.input.isKeyPressed(Input.Keys.UP)){
             grid[row][col].setAngle(90);
+            System.out.println("Up!");
+            this.col += 4;
         }
-		if(Gdx.input.isKeyJustPressed(Input.Keys.DOWN)){
+		if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
             grid[row][col].setAngle(270);
+            System.out.println("Down!");
+            this.col -= 4;
         }
 		
 		if(Gdx.input.isKeyJustPressed(Input.Keys.ENTER)) {
