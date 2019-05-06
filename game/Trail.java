d8ac0c1d5aff6f43eff2a64a0fbfac6b571edd23
Tascate <50305878+Tascate@users.noreply.github.com>
2019-05-06 10:40:23 -0700

    Added changeable length and width

--- game/Trail.java
+++ game/Trail.java
@@ -2,8 +2,14 @@ package com.mygdx.game;
 import com.badlogic.gdx.graphics.Color;
 
 public class Trail extends FieldObject {
+	int length;
+	int width; //Can change with Items/Specials later?
+	int decaySpeed;
 	public Trail(Color c, int a) {
 		light = c;
 		angle = a;
+		length = 7;
+		width = 1;
+		decaySpeed = 4;
 	}
 }
