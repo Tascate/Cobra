50800d27cac6c76007fe8f9ff8ecaa93278333d4
Tascate <50305878+Tascate@users.noreply.github.com>
2019-05-06 10:37:39 -0700

    Update Player.java

--- game/Player.java
+++ game/Player.java
@@ -2,7 +2,10 @@ package com.mygdx.game;
 import com.badlogic.gdx.graphics.Color;
 
 public class Player extends Character {
+	int wins;
+	int losses;
 	public Player(int s,Color c,int a) {
 		super(s,c,a);
+		wins = 0;
 	}
 }
