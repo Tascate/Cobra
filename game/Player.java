a951bf08896351bba2b388216b0ad0754fdb0fad
npham689 <50306821+npham689@users.noreply.github.com>
2019-05-06 11:15:40 -0700

    Made documentations

--- game/Player.java
+++ game/Player.java
@@ -1,8 +1,19 @@
 package com.mygdx.game;
 import com.badlogic.gdx.graphics.Color;
 
+/**
+ * Player class to make a Player and get the features of that player such as their speed, color, and angle.
+ */
 public class Player extends Character {
-	int wins;
+	int wins; //# of wins for the player
+	
+	/**
+	 * Constructor to create the player with their speed, color, and angle. It calls the constructor of the
+	 * super class to get the speed, color, and angle of player. It also sets the number of wins to 0.
+	 * @param s - speed
+	 * @param c - color
+	 * @param a - angle
+	 */
 	public Player(int s,Color c,int a) {
 		super(s,c,a);
 		wins = 0;
