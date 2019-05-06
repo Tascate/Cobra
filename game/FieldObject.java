b30d11a22d40d2618e34637c0eaeb4176f668831
npham689 <50306821+npham689@users.noreply.github.com>
2019-05-06 11:09:38 -0700

    Made the documentations for the class and methods.

--- game/FieldObject.java
+++ game/FieldObject.java
@@ -1,22 +1,42 @@
 package com.mygdx.game;
 import com.badlogic.gdx.graphics.Color;
 
+/**
+ * Abstract class for what things we will have inside our grid, like the characters and the trail along with other features like the color
+ *angle, speed of character.
+ */
 public abstract class FieldObject {
-	protected Color light;
-	protected int angle;
+	protected Color light; //color of character
+	protected int angle; //angle of character, where they changed directions
 	
+	/**
+	 * Method to return the color of the character in the game.
+	 *@return color of character in game.
+	 */
 	public Color getColor() {
 		return light;
 	}
 	
+	/**
+	 * Method to return the angle of the character, where they changed directions.
+	 *@return angle of character, which is where they changed directions.
+	 */	
 	public int getAngle() {
 		return angle;
 	}
 	
+	/**
+	 * Method to set the color of character to the passed in color.
+	 *@param c - color
+	 */
 	public void setColor(Color c) {
 		light = c;
 	}
 	
+	/**
+	 * Method to set the angle of the character to the passed in value.
+	 *@param a - angle value
+         */	
 	public void setAngle(int a) {
 		angle = a;
 	}
