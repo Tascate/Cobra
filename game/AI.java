756ade5a8b479ebcaf52ca429774bde8b9701494
Tascate <50305878+Tascate@users.noreply.github.com>
2019-05-06 10:38:00 -0700

    Update AI.java

--- game/AI.java
+++ game/AI.java
@@ -2,8 +2,10 @@ package com.mygdx.game;
 import com.badlogic.gdx.graphics.Color;
 
 public class AI extends Character {
+	int wins;
 	public AI(int s,Color c,int a) {
 		super(s,c,a);
+		wins = 0;
 	}
 	//AI to be implemented later
 }
