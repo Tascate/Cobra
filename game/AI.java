3424afaa9151b35089c9197f413ceeca55885efa
Tascate <50305878+Tascate@users.noreply.github.com>
2019-05-05 17:01:22 -0700

    Updated to include Angle & Color

--- game/AI.java
+++ game/AI.java
@@ -1,8 +1,9 @@
 package com.mygdx.game;
+import com.badlogic.gdx.graphics.Color;
 
 public class AI extends Character {
-	public AI(int s) {
-		super(s);
+	public AI(int s,Color c,int a) {
+		super(s,c,a);
 	}
 	//AI to be implemented later
 }
