fb3b9edeadfc430260afe5036986a8ea28b6eda8
npham689 <50306821+npham689@users.noreply.github.com>
2019-05-06 11:26:45 -0700

    made documentations so far

--- game/AI.java
+++ game/AI.java
@@ -1,8 +1,18 @@
 package com.mygdx.game;
 import com.badlogic.gdx.graphics.Color;
 
+/**
+ * AI class to make the AI, which is the opponent.
+ */
 public class AI extends Character {
-	int wins;
+	int wins; // number of wins
+	
+	/**
+	 * AI constructor to create the AI, which is the opponent of the other player, and sets their wins to 0.
+	 * @param s - speed of AI
+	 * @param c - color of AI
+	 * @param a - angle of AI
+	 */
 	public AI(int s,Color c,int a) {
 		super(s,c,a);
 		wins = 0;
