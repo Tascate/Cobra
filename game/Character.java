cf3d1bb047a109f4a18d7b9d99fdb7aa44d25a5d
Tascate <50305878+Tascate@users.noreply.github.com>
2019-05-10 11:14:47 -0700

    Initializes Row & Col, LeaveTrail Row and Col

--- game/Character.java
+++ game/Character.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.graphics.Color;
  * Character class to make the characters in the game with the necessary features for the characters. 
  */
 public class Character extends FieldObject {
-	protected boolean alive; //is character alive or not
+	protected boolean alive;
 	
 	/**
 	* Constructor to initialize the instance variable to the passed in values to make the character and add their features.
@@ -13,7 +13,10 @@ public class Character extends FieldObject {
 	*@param c - color of the character
 	*@param a - angle for where the character is changing directions, whether it's going up, down, left, or right.
 	*/
-	public Character(int s, Color c, int a) {
+	public Character(int theRow, int theColumn, int s, Color c, int a) {
+		row = theRow;
+		col = theColumn;
+		
 		speed = s;
 		light = c;
 		angle = a;
@@ -24,8 +27,8 @@ public class Character extends FieldObject {
 	 * Trail method to return the trail that is left behind from the character when the character is currently moving.
 	 *@return the trail left behind from the character along with their color and angle at which they are moving.
 	 */
-	public Trail leaveTrail() {
-		return new Trail(light, angle);
+	public Trail leaveTrail(int row, int col) {
+		return new Trail(row, col, light, angle);
 	}
 	/**
 	 * Method to set the boolean instance variable alive to false, indicating that the character has died.		
@@ -35,7 +38,7 @@ public class Character extends FieldObject {
 	}
 	/**
 	 *Method to return the speed of the character.
-	 *@return speed of character.
+	 *@return speed of characterr.
 	 */
 	public int returnSpeed() {
 		return speed;
