6b0b048de1a9e87197af809e9ffcf770f06c16a9
Tascate <50305878+Tascate@users.noreply.github.com>
2019-05-18 16:01:41 -0700

    Removed Length

--- game/Trail.java
+++ game/Trail.java
@@ -1,4 +1,3 @@
-
 package com.mygdx.game;
 import com.badlogic.gdx.graphics.Color;
 
@@ -6,23 +5,19 @@ import com.badlogic.gdx.graphics.Color;
  * Trail class to make the trail of the player when it is currently moving.
  */
 public class Trail extends FieldObject {
-	int length; //length of trail
 	int width; //Can change with Items/Specials later? // width of trail
 	
 	/**
 	 * Trail constructor to construct the trail of the player and set length to 7 and width to 1.
-	 * @param givenRow - given row
-	 * @param givenColumn - given column
 	 * @param c - color
 	 * @param a - angle
          */	
-	public Trail(int givenRow, int givenColumn, Color c, int a) {
+	public Trail(int givenRow, int givenColumn, Color c, int d) {
 		row = givenRow;
 		col = givenColumn;
 		
 		light = c;
-		angle = a;
-		length = 7;
+		direction = d;
 		width = 1;
 		
 	}
